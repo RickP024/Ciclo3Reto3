06d62a720c8799bfd5a492d87ab05ca879820224
RickP024 <rapr02@hotmail.com>
2021-10-25 21:41:43 -0500

    first commit

---
+++ src/main/java/Domingo_Reto3/Reto3/interfaces/InterfaceReservacion.java
@@ -0,0 +1,9 @@
+package Domingo_Reto3.Reto3.interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+
+import Domingo_Reto3.Reto3.modelos.Reservacion;
+
+public interface InterfaceReservacion extends CrudRepository<Reservacion,Integer>{
+    
+}
